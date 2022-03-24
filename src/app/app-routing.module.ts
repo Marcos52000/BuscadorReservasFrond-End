30d31171af88dc83e9a39b96eae55216334ae87e
PauRobLe <97508709+PauRobLe@users.noreply.github.com>
2022-03-24 13:19:27 +0100

    Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< Updated upstream
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path:'login',component:LoginComponent},
   { path:'register',component:RegisterComponent}
+=======
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+  {
+    path:'admin',component:AdminComponent
+  }
+>>>>>>> Stashed changes
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+=======
+import { AdminComponent } from './admin/admin.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     LoginComponent,
     RegisterComponent
+=======
+    AdminComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
