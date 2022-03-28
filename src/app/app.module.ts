f0fd0e85636121a63c3bb9371251deb3a6d83c03
TheBerayita <97508463+TheBerayita@users.noreply.github.com>
2022-03-28 11:41:06 +0200

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -7,6 +6,9 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/auth/user.service'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  login: any = {
+    email: '',
+    password: ''
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  msg = '';
+  rol: string | null = '';
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+
+    if(window.sessionStorage.getItem("auth-token"))
+    {
+      this.isLoggedIn = true;
+      this.rol = window.sessionStorage.getItem("auth-rol");
+      this.login.email = window.sessionStorage.getItem("auth-email");
+    }
+  }
+
+  logUser(): void {
+    this.userService.login(this.login.email, this.login.password).subscribe(response => {
+
+          window.sessionStorage.setItem("auth-token", response.token);
+          window.sessionStorage.setItem("auth-email", this.login.email);
+
+          this.isLoginFailed = false;
+          this.isLoggedIn = true;
+
+          this.recogerRol();
+        },
+        error => {
+          this.isLoginFailed = true;
+          if("Http failure response for https://buscadorreservas.herokuapp.com/login: 403 OK" == error.message)
+          {
+            this.errorMessage = 'Usuario y/o Contraseña Incorrectos';
+          }
+        });
+  }
+
+  recogerRol(): void
+  {
+    this.userService.getemail(this.login.email)
+    .subscribe(
+      response => {
+        window.sessionStorage.setItem("auth-rol", response.rol.nombre);
+        window.location.reload();
+      },
+      error => {
+        console.log(error.message);
+      }
+    );
   }
 
 }
