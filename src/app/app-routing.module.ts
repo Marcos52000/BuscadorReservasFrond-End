b85f3d9f02929e986a165515630b99b92211917b
Marcos <marcoscousomartin@gmail.com>
2022-03-28 21:08:54 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { HomeComponent } from './home/home.component';
-import { AdminComponent } from './admin/admin.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 
 const routes: Routes = [
   { path:'login',component:LoginComponent},
   { path:'register',component:RegisterComponent},
   { path:"home",component:HomeComponent},
-  { path:"admin", component:AdminComponent}
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+        {
+      path: '',
+      loadChildren: './layouts/admin-layout/admin-layout.module#AdminLayoutModule'
+  }]},
+
 
 
 ];
